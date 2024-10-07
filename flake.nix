b35f82d05367a72efec8f76fd4e74427b551b6ab
cjmakes <cj@cjmakes.com>
2024-10-06 23:19:59 -0700

    adds override comment

--- flake.nix
+++ flake.nix
@@ -20,3 +20,36 @@
         }
     );
 }
+
+# This is also possible using an override
+# {
+# 	description = "A very basic flake";
+# 
+# 	inputs = {
+# 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+# 	};
+# 
+# 	outputs = { self, nixpkgs }: 
+#  let
+# 	system = "x86_64-linux";
+# 	pkgs = import nixpkgs {
+# 		inherit system;
+# 		overlays = [
+# 		(final: prev: {
+# 			python311 = prev.python311.override {
+# 				packageOverrides = pyfinal: pyprev: {
+# 					opencv4 = pyprev.opencv4.override {
+# 					 enableGtk2 = true; 
+# 					};
+# 				};
+# 			};
+# 		})
+# 		];
+# 	};
+# 	in
+# 	{
+# 		devShells.x86_64-linux.default = pkgs.mkShell {
+# 			buildInputs = [ pkgs.python311Packages.opencv4 ];
+# 		};
+# 	};
+# }
