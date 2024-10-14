00abd1d3a65e0504205ac7bdf548e107162eeb42
cjmakes <cj@cjmakes.com>
2024-10-13 22:53:19 -0700

    Do a histogram of pixels instead of average of frame

--- chromalyze.py
+++ chromalyze.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 
-def get_colors(path: str, samplerate: float = 0.03):
+def bin_colors(path: str, samplerate: float = 0.03):
     cap = cv2.VideoCapture(path)
+    bgr = np.zeros((3,256))
 
     if not cap.isOpened():
         print(f"Error: Couldn't open video file {path}")
@@ -13,7 +14,6 @@ def get_colors(path: str, samplerate: float = 0.03):
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     skip = total_frames / (total_frames * samplerate)
     frame_number = 0
-    bgrs = []
     with tqdm(total=total_frames, unit='frames') as pbar:
         while frame_number < total_frames:
             ret, frame = cap.read()
@@ -21,23 +21,30 @@ def get_colors(path: str, samplerate: float = 0.03):
                 print("End of video or error reading frame.")
                 break
 
-            bgrs.append(frame.mean(axis=(0,1)))
+            for row in frame:
+                for pixel in row:
+                    bgr[0][pixel[0]]+=1
+                    bgr[1][pixel[1]]+=1
+                    bgr[2][pixel[2]]+=1
+
             frame_number += skip
             pbar.update(skip)
             cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
 
     cap.release()
-    return np.array(bgrs)
+    return np.array(bgr)
 
-def make_plot(bgrs: any, bins: int = 85):
+def make_plot(bgrs: any, samplerate: float = 0.03):
     clrs = ['blue','green','red']
 
-    bottom = np.zeros(bins)
-    plt.hist(bgrs, bins, color=clrs, stacked=True)
-    plt.title('histogram of average color of frame')
+    bottom = np.zeros(256)
+    for i in range(3):
+        plt.bar(np.arange(256), bgrs[i], width=1.0, label=clrs[i], color=clrs[i], bottom=bottom)
+    plt.title(f"histogram of pixel colors in a {samplerate} sample of frames")
+    plt.yticks([])  
     plt.legend(clrs)
     plt.savefig('hist.png')
 
 if __name__ == "__main__":
     import sys
-    make_plot(get_colors(sys.argv[1]))
+    make_plot(bin_colors(sys.argv[1]))
